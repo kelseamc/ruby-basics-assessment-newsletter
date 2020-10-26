6b27f4a0023eaa91c33de7b82e838169927eaa6e
kelseamc <kelsea.mcall@gmail.com>
2020-10-26 18:06:18 -0400

    fixed syntax line 35-36

--- newsletter.rb
+++ newsletter.rb
@@ -32,7 +32,7 @@ def calculate_recipients
   # write a method that will return an array of only the subscribers who haven't unsubscribed
 end
 
-def first_n_articles(number_of_articles
+def first_n_articles(number_of_articles)
   ARTICLES.first(number_of_articles)
 end
 
